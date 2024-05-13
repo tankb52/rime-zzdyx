7597dc7aea1db02800858cc61301984cd5e2be62
tankb52 <tankb52@163.com>
2024-05-14 03:07:18 +0800

    20240514-030708:更新同步脚本。

--- rsync_rime.sh
+++ rsync_rime.sh
@@ -2,16 +2,22 @@
 
 # * 
 # * File         : rsync_rime.sh
-# * Version      : 0.1
+# * Version      : 0.2
 # * CreateTime   : 2024-05-13 02:30:40
-# * LastEdit     : 2024-05-14 00:54:37
+# * LastEdit     : 2024-05-14 03:04:21
 # * Author       : Knat
 # * Licence      : Copyright © 2024 Knat All Rights Reserved.
-# * Desc         : 用rsync工具备份rime目录。
+# * Desc         : 用 rsync 工具备份 rime 目录。
 # * 
 
+#排除清单
+exclude_list={"'build/'","'pinyin_simp.userdb/'","'sync/'","'zzdyx_userdict.txt'"}
 
 # 同步命令
-rsync -av --exclude-from='/home/tankb51/.local/share/fcitx5/rime/rsync_exclude.txt'  ~/.local/share/fcitx5/rime/  ~/Public/rime-zzdyx.githup/rime-zzdyx/rime
+cmd="rsync -av --exclude=$exclude_list ~/.local/share/fcitx5/rime/  ~/Public/rime-zzdyx.githup/rime-zzdyx/rime"
 
+# 用exec命令会切换路径
+eval $cmd
+
+# 用git命令查看文件更新情况
 git status
